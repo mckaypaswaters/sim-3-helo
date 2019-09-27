b024b13f4c6d1a11406dbd7b139942035b54a9bd
McKay Waters <mckaypaswaters@gmail.com>
2019-09-27 10:45:47 -0600

    styled auth

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import './App.css';
-import Auth from './Components/Auth/Auth'
-import Dashboard from './Components/Dashboard/Dashboard'
-import Form from './Components/Form/Form'
+import routes from './routes'
 import Nav from './Components/Nav/Nav'
-import Post from './Components/Post/Post'
 
-function App() {
+function App(props) {
+  
   return (
     <div className="App">
-      <Auth/>
-      <Dashboard/>
-      <Form/>
-      <Nav/>
-      <Post/>
+      {routes}
+      {/* <Nav/> */}
     </div>
   );
 }
--- src/Components/Auth/Auth.jsx
+++ src/Components/Auth/Auth.jsx
@@ -1,12 +1,34 @@
 import React, {Component} from 'react'
+import wink from '../../assets/wink.png'
+import './auth.css'
 
 class Auth extends Component {
     constructor(){
         super()
+        this.state = {
+            username: '',
+            password: ''
+        }
     }
     render(){
+        console.log(this.props)
         return(
-            <div>Auth</div>
+            <div className="auth-parent">
+                <div className="auth-box">
+                    <img src={wink} alt="winky-logo"/>
+                    <h1>Helo</h1>
+                    <div className='input-box'>
+                        Username:<input type="text"/>
+                    </div>
+                    <div className='input-box'>
+                        Password:<input type="password"/>
+                    </div>
+                    <nav className="login-register-buttons">
+                        <button className='login'>Login</button>
+                        <button className='register'>Register</button>
+                    </nav>
+                </div>
+            </div>
         )
     }
 }
