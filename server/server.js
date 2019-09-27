06831f96a03fc7a9c363ed35ccd750ae223ac22d
McKay Waters <mckaypaswaters@gmail.com>
2019-09-27 09:48:11 -0600

    massive set up

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
-const {SERVER_PORT} = process.env
+const {SERVER_PORT, CONNECTION_STRING} = process.env
 const ctrl = require('./controller')
 const app = express()
 
 app.use(express.json())
 
-app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} Days til Halo Infinite`))
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} Days til Halo Infinite`))
+})
